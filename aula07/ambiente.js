d54e2690983fe933f4350b44b05ec705ff676e70
Leandro <now11@outlook.com>
2025-12-29 09:57:43 -0300

    Repetições em JavaScript

---
+++ aula07/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+var d = 1
+
+while (c <= 5){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+do {
+    console.log(`Passos ${c}`)
+    c++
+} while (c <= 5)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
