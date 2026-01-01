2d7b48b509716b395e2747967ec03c1ea51b814f
Leandro <now11@outlook.com>
2026-01-01 18:48:47 -0300

    Vari

---
+++ aula09/ambiente2.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3, 4]
+
+num.push(1)
+
+console.log(num)
+
+let pos = num.indexOf(2)
+
+if (pos == -1){
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
