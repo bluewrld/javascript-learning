ac188b413728a7abe6ac889724e5c2c8c3d571a0
Leandro <now11@outlook.com>
2026-01-05 16:25:32 -0300

    Objeto Math em JavaScript

---
+++ fundamentos/aula012/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.50;
+// let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1)
+//let num2 = Math.round(num1);
+//let num2 = Math.max(1, 2, 3, 1500, 900, 200, 300 , 400, 300)
+//let num2 = Math.min(1, 2, 3, 1500, 900, 200, 300 , 400, 300)
+const aleatorio = Math.floor(Math.random() * 101)
+
+console.log(aleatorio)
