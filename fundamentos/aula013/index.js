379041c85b6283c7e21c528bc0199acbf39fec82
Leandro <now11@outlook.com>
2026-01-07 16:51:50 -0300

    Array em JavaScript

--- fundamentos/aula013/index.js
+++ fundamentos/aula013/index.js
@@ -1,6 +1,16 @@
 const alunos = ['Luiz', 'Maria', 'João']
 
-alunos.push('Luiza')
+// delete alunos[1]
+// console.log(alunos[1])
 
-console.log(alunos.length)
-console.log(alunos)
+// console.log(alunos.length)
+// alunos.unshift('Leo')
+// alunos.push('Luiza')
+
+alunos.push('Eduardo')
+alunos.push('José')
+
+
+console.log(alunos.slice(0, 3))
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
