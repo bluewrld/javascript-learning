8f47d8d9206baa296cb0cb62a34394b83d8ba2cc
Leandro <now11@outlook.com>
2026-01-11 18:57:50 -0300

    Exercicio com funções e Objetos em JavaScript

--- fundamentos/exercicios/ex32/script.js
+++ fundamentos/exercicios/ex32/script.js
@@ -1,10 +1,34 @@
 function meuEscopo () {
+    const res = document.getElementById('res')
     const form = document.querySelector('.form')
 
-    form.onsubmit = function (evento){
-        evento.preventDefault()
-        alert(1)
+    const pessoas = []
+
+    function recebeEventoForm (e) {
+        e.preventDefault()
+
+        const nome = document.querySelector('.nome')
+        const sobrenome = document.querySelector('.sobrenome')
+        const peso = document.querySelector('.peso')
+        const altura = document.querySelector('.altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+
+        console.log(pessoas)
+
+        res.innerHTML += `<p>${nome.value} ${sobrenome.value}</p>`
+        res.innerHTML += `<p>Peso: ${peso.value}KG</p>`
+        res.innerHTML += `<p>Altura: ${altura.value}</p>`
     }
+
+    form.addEventListener('submit', recebeEventoForm)
+
+    
 }
 
 meuEscopo()
