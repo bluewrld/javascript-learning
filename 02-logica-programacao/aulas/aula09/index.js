0f5d8a6808e534a9b93eec7fd2b8582e0066a64b
Leandro <now11@outlook.com>
2026-01-19 17:10:24 -0300

    Diferença entre var e let/const

--- 02-logica-programacao/aulas/aula09/index.js
+++ 02-logica-programacao/aulas/aula09/index.js
@@ -1,9 +1,20 @@
 const verdadeira = true
 
+// Let tem escopo de bloco { ... bloco}
+// Var só tem escopo de função
+
 let nome = 'Leo'
 var nome2 = 'Mota'
 
 
 if (verdadeira) {
+    let nome = 'Blue' // criando
     console.log(nome, nome2)
+
+    if (verdadeira) {
+        var nome2 = 'Leandro'
+        let nome = 'Outra coisa'
+        console.log(nome, nome2)
+    }
 }
+
