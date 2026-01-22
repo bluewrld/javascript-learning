b784d0b04e67ba946b6f23d76a86783aac52d228
Leandro <now11@outlook.com>
2026-01-21 21:31:34 -0300

    For - Clássico - Estrutura de Repetição em JavaScript

---
+++ 02-logica-programacao/aulas/aula12/index.js
@@ -0,0 +1,10 @@
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Melão', 'Melancia', 'Manga', 'Morango'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`,frutas[i])
+}
+
+// for (let i = 0 ; i <= 10 ; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par);
+// }
