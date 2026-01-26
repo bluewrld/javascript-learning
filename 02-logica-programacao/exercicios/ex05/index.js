c77335a0aa2d8ff0ed3d2dbf65b33e84a44b92cd
Leandro <now11@outlook.com>
2026-01-26 12:04:44 -0300

    Exercício com Lógica de Programação em JavaScript

---
+++ 02-logica-programacao/exercicios/ex05/index.js
@@ -0,0 +1,12 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function max(x, y){
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+
+console.log(max(1, 50));
