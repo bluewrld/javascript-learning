5676c8f8c61df2225767d2f965b3c8f34ad23444
bluewrld <now11@outlook.com>
2026-02-03 22:04:36 -0300

    As várias maneiras de declarar funções em JavaScript

--- 03-funcoes-avancado/aulas/aula01/index.js
+++ 03-funcoes-avancado/aulas/aula01/index.js
@@ -2,5 +2,18 @@
 
 falaOi();
 function falaOi(){
-    console.log('Oie')
+    console.log('Oi');
 }
+
+// First-class objects (Objetos de primeira classe
+
+const souUmDado = function() {
+console.log('Sou um dado.');
+};
+souUmDado( ) ;
+
+// Arrow function
+const funcaoArrow = () => {
+};
+console.log('Sou uma arrow function');
+funcaoArrow();
