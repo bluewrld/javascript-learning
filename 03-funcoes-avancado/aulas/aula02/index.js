d876305807f24e2e986e5dfb336110d84e334ade
bluewrld <now11@outlook.com>
2026-02-11 22:50:34 -0300

    Parâmetros da função em JavaScript

--- 03-funcoes-avancado/aulas/aula02/index.js
+++ 03-funcoes-avancado/aulas/aula02/index.js
@@ -1,5 +1,22 @@
 // argumentos que sustenta todos os argumentos enviados
-function funcao(a, b = 2, c = 4) {
-    console.log(a + b + c)
-}
-funcao(1, undefined, 20);
+// function funcao([valor1, valor2, valor3]) {
+//     console.log(valor1, valor2, valor3);
+// }
+// funcao(['BlueWRLD', 'Desenvolvedor', '24']);
+
+
+// function conta(operador, acumulador, ...numeros) {
+//     for(let numero of numeros) {
+//         if (operador === '+') acumulador += numero;
+//         if (operador === '-') acumulador -= numero;
+//         if (operador === '/') acumulador /= numero;
+//         if (operador === '*') acumulador *= numero;
+//         console.log(acumulador);
+//     }
+// }
+// conta('-', 200, 20, 30, 40, 50);
+
+const conta = (...args) => {
+    console.log(args)
+};
+conta('+', 1, 20, 30, 40, 50);
