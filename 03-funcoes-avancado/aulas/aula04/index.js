e249ccd219e8f9c711b5078ba6eb3f2f6cc4ec50
bluewrld <now11@outlook.com>
2026-02-14 11:25:10 -0300

    Escopo léxico em JavaScript

--- 03-funcoes-avancado/aulas/aula04/index.js
+++ 03-funcoes-avancado/aulas/aula04/index.js
@@ -1,1 +1,11 @@
-console.log('Oi');
+const nome = 'BlueWRLD';
+
+function falaNome() {
+    console.log(nome);
+}
+
+function usaFalaNome() {
+    const nome = 'WRLD';
+    falaNome();
+}
+usaFalaNome();
