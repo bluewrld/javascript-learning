346c8118358b52a1c7da8480900f7678ed707b18
bluewrld <now11@outlook.com>
2026-02-16 18:46:06 -0300

    Funções imediatas (IIFE) em JavaScript

---
+++ 03-funcoes-avancado/aulas/aula07/index.js
@@ -0,0 +1,16 @@
+// IIFE -> Immediately invoked function expression
+(function(idade, profissao) {
+    const sobreNome = 'WRLD';
+    function criaNome(nome) {
+        return nome + ' ' + sobreNome;
+    }
+
+    function falaNome() {
+        console.log(criaNome('BLUE'));
+    }
+
+    falaNome();
+    console.log(idade, profissao)
+})(24, 'desenvolvedor');
+
+const nome = 'Qualquer coisa';
