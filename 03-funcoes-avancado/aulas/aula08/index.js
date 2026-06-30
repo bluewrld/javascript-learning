ff82bd49abaf89ce74905d393840aa361536ba50
bluewrld <now11@outlook.com>
2026-06-30 20:02:38 -0300

    Funções fábrica (Factory Functions) em JavaScript

--- 03-funcoes-avancado/aulas/aula08/index.js
+++ 03-funcoes-avancado/aulas/aula08/index.js
@@ -1,13 +1,23 @@
-function criaPessoa(nome, profissao, altura, peso) {
+// Factory function (Função Fábrica)
+// Constructor function (Função construtora)
+
+function criaPessoa(nome, sobrenome, a, p) {
     return {
-        nome, profissao,
+        nome,
+        sobrenome,
         fala: function(assunto) {
-            return `${this.nome} está falando ${assunto}.`;
+            return `${this.nome} está ${assunto}.`;
         },
-        altura,
-        peso
+        altura: a,
+        peso: p,
+        get imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa('Blue', 'Desenvolvedor');
-console.log(p1.fala(`que sua profissão é ${p1.profissao}`));
+const p1 = criaPessoa('Blue', 'WLRD', 1.8, 80);
+console.log(p1.imc);
+const p2 = criaPessoa('Maria', 'Joaquina', 1.6, 42);
+console.log(p2.imc);
