65e13a4e098ea1d061fdcfab95ae534d2aac299e
bluewrld <now11@outlook.com>
2026-07-03 19:54:08 -0300

    Funções construtoras (constructor functions) em JavaScript

---
+++ 03-funcoes-avancado/aulas/aula10/main.js
@@ -0,0 +1,26 @@
+// Função construtora -> objetos
+// Função fabrica -> objetos
+// Construtora -> Pessoa (New)
+
+function Pessoa(nome, sobrenome) {
+    // Atributos ou métodos privados
+    const ID = 123456;
+
+    const metodoInterno = function() {
+        
+    };
+
+    // Atributos ou métodos públicos
+
+    this.nome = nome
+    this.sobrenome = sobrenome
+
+    this.metodo = function() {
+        console.log(this.nome + ': Sou um método');
+    };
+}
+
+const p1 = new Pessoa('Luiz', 'Otavio');
+const p2 = new Pessoa('Maria', 'Oliveira')
+
+p2.metodo();
