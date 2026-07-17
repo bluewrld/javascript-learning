9d2f19e95945f8157a001f7d7766008e2c8c329c
bluewrld <now11@outlook.com>
2026-07-17 17:11:04 -0300

    Revisão do básico em Arrays JavaScript

---
+++ 04-arrays-avancado/aulas/aula01/main.js
@@ -0,0 +1,11 @@
+const nomes = ['Karine', 'Maria', 'Andressa', 'Cookie', 'Doritos'];
+const novo = nomes.slice(0, -1);
+console.log(novo);
+
+const frutas = 'Maçã Banana Uva';
+const fruta = frutas.split(' ');
+console.log(fruta);
+
+const listas = ['PowerBank', 'Bolsa', 'Garrafa'];
+const lista = listas.join(', ');
+console.log(lista);
