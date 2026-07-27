3c0a5edb4324eace0597e56d42ea2df81e3bf569
bluewrld <now11@outlook.com>
2026-07-27 15:25:46 -0300

    Reduce - Reduzindo o array em JavaScript

--- 04-arrays-avancado/aulas/aula06/main.js
+++ 04-arrays-avancado/aulas/aula06/main.js
@@ -8,7 +8,30 @@ const total = numeros.reduce(function(acumulador, valor) {
     return acumulador;
 }, 0);
 
-const soma = numeros.reduce(acumulador, valor => acumulador += valor);
-console.log(soma);
+const soma = numeros.reduce((acumulador, valor) => acumulador + valor, 0);
+const pares = numeros.reduce((acumulador, valor) => (valor % 2 === 0 && acumulador.push(valor), acumulador), []);
+const dobro = numeros.reduce((acumulador, valor) => (acumulador.push(valor * 2), acumulador), []);
 
-console.log(total);
+console.log(soma);
+console.log(pares);
+console.log(dobro);
+
+
+// Retorne a pessoa mais velha
+const pessoas = [
+    {nome: 'Karine', idade: 17},
+    {nome: 'Leo', idade: 24},
+    {nome: 'Dessa', idade: 23},
+    {nome: 'Jose', idade: 66},
+    {nome: 'Tranqueira', idade: 27},
+    {nome: '', idade: 1},
+];
+
+// const maisVelha = pessoas.reduce(function(acumulador, valor) {
+//     if(acumulador.idade > valor.idade) return acumulador;
+//     return valor;
+// });
+// console.log(maisVelha);
+
+const maisVelha = pessoas.reduce((acumulador, valor) => { if (acumulador.idade > valor.idade) return acumulador; return valor; });
+console.log(maisVelha);
