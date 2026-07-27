a128309a9c65a6b26bd19d1b06d8e9647cd4137b
bluewrld <now11@outlook.com>
2026-07-27 16:00:27 -0300

    Filter + Map + Reduce em JavaScript

---
+++ 04-arrays-avancado/aulas/aula07/main.js
@@ -0,0 +1,17 @@
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+
+// const numerosPares = numeros.filter(function(valor) {
+//     return valor % 2 === 0;
+// }).map(function(valor) {
+//     return valor *2;
+// }).reduce(function(acc, v) {
+//     return acc + v;
+// });
+
+const Pares = numeros.filter(valor => valor % 2 === 0).map(valor => valor *2).reduce((acc, valor) => acc + valor);
+console.log(Pares);
